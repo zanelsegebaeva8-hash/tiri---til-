61e28840061e64e42b3977438db8223d3e2f5696
zanelsegebaeva8-hash <zanelsegebaeva8@gmail.com>
2025-10-25 23:21:46 +0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,48 @@
+body {
+  font-family: "Segoe UI", sans-serif;
+  background-color: #f9f9f9;
+  margin: 0;
+  padding: 0;
+  color: #333;
+  text-align: center;
+}
 
+header {
+  background-color: #6cb98f;
+  color: white;
+  padding: 20px 0;
+}
+
+.menu {
+  margin: 20px;
+}
+
+.menu button {
+  margin: 10px;
+  padding: 12px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: #b2dfdb;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.menu button:hover {
+  background-color: #80cbc4;
+}
+
+.content {
+  margin: 30px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+}
+
+footer {
+  background-color: #6cb98f;
+  color: white;
+  padding: 10px 0;
+  margin-top: 30px;
+}
